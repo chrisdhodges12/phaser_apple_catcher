a7971aa537c8e5e1a865e056a9b7480da142aa11
chrisdhodges12 <102702357+chrisdhodges12@users.noreply.github.com>
2024-01-13 14:09:38 -0500

    add apple, falls at constant fate, random x direction

--- main.js
+++ main.js
@@ -14,11 +14,14 @@ class GameScene extends Phaser.Scene{
     this.player
     this.cursor
     this.playerSpeed=speedDown+50
+    this.target
   }
 
   preload(){
     this.load.image("bg", "/assets/bg.png")
     this.load.image("basket", "/assets/basket.png")
+    this.load.image("apple", "/assets/apple.png")
+
   }
 
   create(){
@@ -26,10 +29,22 @@ class GameScene extends Phaser.Scene{
     this.player = this.physics.add.image(0,sizes.height-100,"basket").setOrigin(0,0)
     this.player.setImmovable(true)
     this.player.body.allowGravity = false
+    this.player.setCollideWorldBounds(true)
+
+    this.target = this.physics.add.image(0,0, "apple").setOrigin(0, 0);
+    this.target.setMaxVelocity(0, speedDown);
+
+
     this.cursor=this.input.keyboard.createCursorKeys()
   }
 
   update(){
+
+    if (this.target.y >= sizes.height) {
+      this.target.setY(0);
+      this.target.setX(this.getRandomX());
+    }
+
     const {left,right}= this.cursor
 
     if (left.isDown) {
@@ -40,6 +55,10 @@ class GameScene extends Phaser.Scene{
      else {
       this.player.setVelocityX(0);
      }
+  }
+
+  getRandomX() {
+    return Math.floor(Math.random() * 480);
   }
 
 }
@@ -60,4 +79,3 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
-
